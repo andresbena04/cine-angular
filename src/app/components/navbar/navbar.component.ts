cdc9baaa411b945a5f64895eeab8dc0d5b418887
Andres Benavides <andresbena3515@gmail.com>
2024-04-20 23:17:54 -0500

    class active de la ruta

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  constructor(private router:Router){}
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
